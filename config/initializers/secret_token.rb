3a1451e4331ebdc33f1134488ce0fa3b68e35825
Andrew Harris <buzzbuzz@harrisfamilyfour.com>
2012-09-12 13:13:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindingsDatabase::Application.config.secret_token = '1e96148c3011c32310acb72921df837bf9358be32bc6f5c34322ee988883fc5eb8be3349a9b913304e20cba110dc37a6ce0e08ada7bb3455d5ec5850835ae187'
